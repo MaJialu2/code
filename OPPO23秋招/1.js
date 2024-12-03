e441cc5aaf74aaba4ae15e47db4519a22162aa23
mjl <2404816298@qq.com>
2024-12-03 13:32:12 +0800

    13题完成

--- OPPO23秋招/1.js
+++ OPPO23秋招/1.js
@@ -1,12 +1,12 @@
 let obj1 = {
-  name: "张三",
+  name: '张三',
   getName() {
     return this.name;
   },
 };
 
 let obj2 = {
-  name: "李四",
+  name: '李四',
   getName() {
     return super.getName();
   },
@@ -14,3 +14,5 @@ let obj2 = {
 
 Object.setPrototypeOf(obj2, obj1);
 console.log(obj2.getName());
+
+// super 调用时的上下文仍然是当前对象，即this 指向当前对象。
